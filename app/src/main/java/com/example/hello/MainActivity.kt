7aa99288da4634af803c33a6cfa50c784e3a7eb9
EuniceMusenyia <eunicemusenyia@gmail.com>
2023-04-27 23:17:34 +0300

    added Login Activity

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.hello
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var TVlogin: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        TVlogin =findViewById(R.id.TVlogin)
+        TVlogin.setOnClickListener {
+            val intent = Intent(this,LOGIN2::class.java)
+            startActivity(intent)
+        }
     }
 }
